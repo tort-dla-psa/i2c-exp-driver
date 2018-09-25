da5e80fbfc1ab9eeef4596765b6ea399a8355ea3
lab <lab@DESKTOP-SF535DB>
2018-09-25 18:39:13 +0300

    Made testing app for i2c and oled

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-#include "../include/oled-exp.h"
+#include "include/oled-exp.h"
 
 int main()
 {
-	std::cout << "Testing FastOledExp\a";
+	std::cout << "Testing FastOledExp\n";
+	fastOledDriver drv = fastOledDriver();
+	drv.init();
+	if (!drv.checkInit()) {
+		std::cout << "Failed init\n";
+		return -1;
+	}
+	drv.setCursor(1, 0);
+	drv.write("Hello world\n Hi there");
     return 0;
 }
